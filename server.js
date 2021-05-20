1045c0eeb4393c9c4754e9130538fe63eb2e718d
atbeesley <alexander.beesley@gmail.com>
2021-05-20 15:31:03 +0100

    working in browser

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => { console.log('hello from node')})
+const server = http.createServer((req, res) => { 
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Hi from the browser</h1>')
+    res.end()
+})
 
 const PORT = process.env.PORT || 5000 
 
