60fb2d76a2d2984e37652434e207f8c9a4ba2256
atbeesley <alexander.beesley@gmail.com>
2021-05-22 09:29:47 +0100

    find productById working

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,7 +10,22 @@ async function getProducts(req, res){
     }
 }
 
+async function getProduct(req, res, id){
+    try {
+        const product = await Product.findById(id)
+        if(!product){
+            res.writeHead(404, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify({ message: 'product not found'}))
+        } else {
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify(product))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = { 
-    getProducts
+    getProducts, getProduct
 }
 
