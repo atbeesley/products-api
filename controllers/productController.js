45e7dbd21f88eb9c95c9d056a67a7ca8efe0119c
atbeesley <alexander.beesley@gmail.com>
2021-05-26 12:40:49 +0100

    delete functionality working

--- controllers/productController.js
+++ controllers/productController.js
@@ -75,10 +75,30 @@ async function updateProduct(req, res, id){
     }
 }
 
+async function deleteProduct(req, res, id){
+    try {
+
+        const product = await Product.findById(id)
+
+        if(!product){
+            res.writeHead(404, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify({ message: 'product not found'}))
+        } else {
+            await Product.remove(id)
+            res.writeHead(200, {'Content-Type':'application/json'})
+            res.end(JSON.stringify({ message: `Product ${id} removed`}))
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = { 
     getProducts,
     getProduct, 
     createProduct, 
-    updateProduct
+    updateProduct,
+    deleteProduct
 }
 
